d4c8a9740c4c69cc51ca7ca7103370078c3a2501
yirano <yirano@protonmail.ch>
2019-11-21 13:32:02 -0500

    Add footer contents and style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-import Header from "./components/header/header";
-import Jumbotron from "./components/jumbotron/jumbotron";
-import Statistics from "./components/statistics/statistics";
-import "./App.scss";
+import React from 'react';
+import Header from './components/header/header';
+import Jumbotron from './components/jumbotron/jumbotron';
+import Statistics from './components/statistics/statistics';
+import Cta from './components/cta/cta';
+import Footer from './components/footer/footer';
+import './App.scss';
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <Jumbotron />
-      <Statistics />
-    </main>
-  );
+	return (
+		<div className="container">
+			<main>
+				<Header />
+				<Jumbotron />
+				<Statistics />
+				<Cta />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
