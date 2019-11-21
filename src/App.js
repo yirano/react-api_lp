2529c70bec4d2ee894916cfd724321959726cee3
yirano <yirano@protonmail.ch>
2019-11-21 08:51:26 -0500

    Style header and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header/header';
+import Jumbotron from './components/jumbotron/jumbotron';
 import './App.scss';
 
 function App() {
 	return (
 		<main>
 			<Header />
+			<Jumbotron />
 		</main>
 	);
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,25 +4,25 @@ import './header.scss';
 export default function header() {
 	return (
 		<header>
-			<h1>Shortly</h1>
+			<h2>Shortly</h2>
 			<nav>
 				<div className="navLeft">
 					<ul>
 						<li>
-							<a href="#">Features</a>
+							<a href="/">Features</a>
 						</li>
 						<li>
-							<a href="#">Pricing</a>
+							<a href="/">Pricing</a>
 						</li>
 						<li>
-							<a href="#">Resources</a>
+							<a href="/">Resources</a>
 						</li>
 					</ul>
 				</div>
 				<div className="navRight">
 					<ul>
 						<li>
-							<a href="#">Login</a>
+							<a href="/">Login</a>
 						</li>
 						<li>
 							<button className="btn-signUp">Sign Up</button>
