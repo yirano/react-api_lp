2120f2b5d071a540ed1d6bc4608991a905073551
yirano <yirano@protonmail.ch>
2019-11-20 22:02:45 -0500

    Begin styling header nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,6 +5,31 @@ export default function header() {
 	return (
 		<header>
 			<h1>Shortly</h1>
+			<nav>
+				<div className="navLeft">
+					<ul>
+						<li>
+							<a href="#">Features</a>
+						</li>
+						<li>
+							<a href="#">Pricing</a>
+						</li>
+						<li>
+							<a href="#">Resources</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navRight">
+					<ul>
+						<li>
+							<a href="#">Login</a>
+						</li>
+						<li>
+							<button className="btn-signUp">Sign Up</button>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		</header>
 	);
 }
