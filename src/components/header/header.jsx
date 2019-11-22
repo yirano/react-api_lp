571cd2b85412e028a727c26f7d5d23315775f6a9
yirano <yirano@protonmail.ch>
2019-11-22 13:22:51 -0500

    Add menu icon button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import './header.scss';
+import React from "react";
+import "./header.scss";
 
 export default function header() {
-	return (
-		<header>
-			<h2>Shortly</h2>
-			<nav>
-				<div className="navLeft">
-					<ul>
-						<li>
-							<a href="/">Features</a>
-						</li>
-						<li>
-							<a href="/">Pricing</a>
-						</li>
-						<li>
-							<a href="/">Resources</a>
-						</li>
-					</ul>
-				</div>
-				<div className="navRight">
-					<ul>
-						<li>
-							<a href="/">Login</a>
-						</li>
-						<li>
-							<button className="btn-signUp">Sign Up</button>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <h2>Shortly</h2>
+      <button className="menuIcon">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav className="show">
+        <div className="navLeft">
+          <ul>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Resources</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navRight">
+          <ul>
+            <li>
+              <a href="/">Login</a>
+            </li>
+            <li>
+              <button className="btn-signUp">Sign Up</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
