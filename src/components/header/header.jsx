29bd53e388e5d4be508dbb562f6808df16838dc0
yirano <yirano@protonmail.ch>
2019-11-23 21:22:11 -0500

    Animate third span child in responsive menu icon

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,40 +1,57 @@
-import React from "react";
-import "./header.scss";
+import React, { Component } from 'react';
+import './header.scss';
 
-export default function header() {
-  return (
-    <header>
-      <h2>Shortly</h2>
-      <button className="menuIcon">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <nav className="show">
-        <div className="navLeft">
-          <ul>
-            <li>
-              <a href="/">Features</a>
-            </li>
-            <li>
-              <a href="/">Pricing</a>
-            </li>
-            <li>
-              <a href="/">Resources</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navRight">
-          <ul>
-            <li>
-              <a href="/">Login</a>
-            </li>
-            <li>
-              <button className="btn-signUp">Sign Up</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
+export class header extends Component {
+	state = {
+		show: false
+	};
+
+	handleClick = () => {
+		this.setState((prevState) => {
+			return (prevState.show = !prevState.show);
+		});
+	};
+
+	render() {
+		return (
+			<header>
+				<h2>Shortly</h2>
+				<button
+					className={this.state.show ? 'menuIcon menuActive' : 'menuIcon menuInactive'}
+					onClick={this.handleClick}
+				>
+					<span />
+					<span />
+					<span />
+				</button>
+				<nav className={this.state.show ? 'show' : null}>
+					<div className="navLeft">
+						<ul>
+							<li>
+								<a href="/">Features</a>
+							</li>
+							<li>
+								<a href="/">Pricing</a>
+							</li>
+							<li>
+								<a href="/">Resources</a>
+							</li>
+						</ul>
+					</div>
+					<div className="navRight">
+						<ul>
+							<li>
+								<a href="/">Login</a>
+							</li>
+							<li>
+								<button className="btn-signUp">Sign Up</button>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+		);
+	}
 }
+
+export default header;
--- src/components/jumbotron/jumbotron.jsx
+++ src/components/jumbotron/jumbotron.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './jumbotron.scss';
 
-export default function jumbotron() {
-	return (
-		<section className="jumbotron">
-			<div className="top-jumbotron">
-				<div className="leftJumbo">
-					<h1>More than just shorter links</h1>
-					<p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
-					<button className="btn-jumbotron">Get Started</button>
+export class jumbotron extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+
+	// componentDidMount() {
+	//   fetch('https://rel.ink/api/links/', {
+	//     method: 'POST',
+	//     body: 'https://www.youtube.com/watch?v=hzLDsxPGctY'
+	//   })
+	// }
+
+	render() {
+		let { isLoaded, items } = this.state;
+		return (
+			<section className="jumbotron">
+				<div className="top-jumbotron">
+					<div className="leftJumbo">
+						<h1>More than just shorter links</h1>
+						<p>
+							Build your brand's recognition and get detailed insights on how your links are performing.
+						</p>
+						<button className="btn-jumbotron">Get Started</button>
+					</div>
+					<div className="rightJumbo" />
 				</div>
-				<div className="rightJumbo" />
-			</div>
-			<div className="bottom-jumbotron">
-				<input type="text" id="text" placeholder="Shorten a link here..." />
-				<input type="submit" value="Shorten it!" />
-			</div>
-		</section>
-	);
+				<div className="bottom-jumbotron">
+					<input type="text" id="text" placeholder="Shorten a link here..." />
+					<input type="submit" value="Shorten it!" />
+				</div>
+			</section>
+		);
+	}
 }
+
+export default jumbotron;
