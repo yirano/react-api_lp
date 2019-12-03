74e0b9195e50b923e6ca3ae2546c21b2a6d2dd00
yirano <yirano@protonmail.ch>
2019-12-03 17:35:08 -0500

    Remove 'top jumbotron' styling and combine with overall jumbotron styling

--- src/components/jumbotron/jumbotron.jsx
+++ src/components/jumbotron/jumbotron.jsx
@@ -14,16 +14,12 @@ export class jumbotron extends Component {
 	render() {
 		return (
 			<section className="jumbotron">
-				<div className="top-jumbotron">
-					<div className="leftJumbo">
-						<h1>More than just shorter links</h1>
-						<p>
-							Build your brand's recognition and get detailed insights on how your links are performing.
-						</p>
-						<button className="btn-jumbotron">Get Started</button>
-					</div>
-					<div className="rightJumbo" />
+				<div className="leftJumbo">
+					<h1>More than just shorter links</h1>
+					<p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+					<button className="btn-jumbotron">Get Started</button>
 				</div>
+				<div className="rightJumbo" />
 			</section>
 		);
 	}
